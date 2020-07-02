ffc2deada75d6ea8faeae349dbaec39049349d3f
shenyan2018 <shenyan2018@chaoxing.com>
2020-07-02 09:59:41 +0800

    basic pro

---
+++ pro_bussiness/src/main/java/com/my/spring_mvc_pro/interfaceController/InterfacePage.java
@@ -0,0 +1,24 @@
+package com.my.spring_mvc_pro.interfaceController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+@RequestMapping("/interface")
+public class InterfacePage {
+    @RequestMapping("/001")
+    @ResponseBody
+    public String getParamByGet(
+            @RequestParam(value="username", required=true)String name,
+            @RequestParam(value="usersex",required=false)String sex,
+            @RequestParam(value="remark",required=false)String remark
+                               ) {
+        System.out.println(name + " " + sex+" "+remark);
+        System.out.println("success 001");
+        return "success";
+    }
+
+}
---
+++ pro_bussiness/src/main/java/com/my/spring_mvc_pro/pagesController/HomePage.java
@@ -0,0 +1,16 @@
+package com.my.spring_mvc_pro.pagesController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/homepage")
+public class HomePage {
+
+    @RequestMapping("001")
+    public String sayHello() {
+        //System.out.println("success 001");
+        return "/HomePage/homepage";
+    }
+}
